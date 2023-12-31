7a5ae6b8c290b6039079d5a250471c6126f88fac
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2023-12-31 12:32:38 +0530

    Add files via upload

---
+++ Night_Sky_explorer/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const exploreButton = document.querySelector('.explore-button');
+
+    exploreButton.addEventListener('click', function () {
+        window.location.href = 'constellations.html'; // Replace with the actual name of your constellations page
+    });
+});
